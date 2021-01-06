ef9ed406fc2cd72505828fa7e8c9d1c24dc6b7f4
vivi <ashin-jo@yahoo.com.tw>
2021-01-06 22:55:49 +0800

    20210106 final

--- QA 20201228/ViewController.swift
+++ QA 20201228/ViewController.swift
@@ -18,7 +18,7 @@ struct music: Codable {
     let singer: String
     let musicURL: URL
     let image: [URL]
-    
+//    宣告圖片的進階寫法
     init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             name = try container.decode(String.self, forKey: .name)
@@ -51,7 +51,6 @@ extension music {
     }
 }
 
-
 class ViewController: UIViewController {
     //設定取用data
         var musics = music.data
@@ -61,66 +60,59 @@ class ViewController: UIViewController {
     
 //   定義問題
     @IBOutlet weak var questionLabel: UILabel!
+   
 //    定義答案
     @IBOutlet weak var answerLabel: UILabel!
+    
     @IBOutlet weak var answerSingerLabel: UILabel!
 //    定義答案歌手顯示照片
     @IBOutlet weak var singerImage: UIImageView!
     
-
-
 //    播放音樂的動作定義
-//    var player:AVAudioPlayer = AVAudioPlayer()
     let player = AVPlayer()
     @IBAction func play(_ sender: Any) {
+    autoplay()
+    }
+//    設定自動播放功能
+    func autoplay() {
         let fileUrl = musics[random].musicURL
         let playerItem = AVPlayerItem(url: fileUrl)
         player.replaceCurrentItem(with: playerItem)
         player.play()
-        print(random)
     }
     @IBAction func pause(_ sender: Any) {
         player.pause()
     }
     @IBAction func restart(_ sender: Any) {
 //        player.stop()
-        player.seek(to: .zero) //因為stop音樂功能好像不支援了 改成寫指定到時間0
+        player.seek(to: .zero) //因為stop音樂功能不支援了 改成寫指定到時間0
         player.play()
     }
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        let question = Question(description: "Play the Music!", answer: "musics[0].name"
-//        questions.append(question1)
-//
-//        questions.shuffle()
-//
-//        questionLabel.text = questions[index].description
-//        answerLabel.text = ""
-
     }
-
+//設定點選顯示答案要出現的內容
     @IBAction func showAnswerBtn(_ sender: UIButton) {
         answerLabel.text = musics[random].name
         answerSingerLabel.text = musics[random].singer
-//        singerImage. = UIImage(data: musics[random].image)
+        singerImage.image = try? UIImage(data: Data(contentsOf: musics[random].image[0]))
         
     }
+//    設定點選下一首的功能跟動作
     @IBAction func nextBtn(_ sender: UIButton) {
-        
-////        if index < questions.count - 1 {
-//        index = index + 1
-//            if index == questions.count{
-//                index = 0
-//            }
-//        questionLabel.text = questions[index].description
-//        answerLabel.text = ""
+        player.pause()
+        random = Int.random(in: 0...44)
+        autoplay()
+        answerLabel.text = ""
+        answerSingerLabel.text = ""
+        singerImage.image = UIImage(named: "microphone")
+
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
     }
 }
 
